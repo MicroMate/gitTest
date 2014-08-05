f4f95d7c5b8c45c1aed35ecd3fa4541dec4c6631
Michal Matczak <matczak.wln@gmail.com>
2014-08-06 00:21:59 +0200

    Dodalem klase KlasaNowyBranch

--- Hello_EGit/src/com/micromate/hello_egit/Hello_eGit.java
+++ Hello_EGit/src/com/micromate/hello_egit/Hello_eGit.java
@@ -3,8 +3,13 @@ package com.micromate.hello_egit;
 public class Hello_eGit {
 	 
 	
-	 static void main (String args[]){ 
+	public static void main (String args[]){ 
 	
 		System.out.println("Hello eGIT");
+		
+		//dodane w nowym branchu
+		KlasaNowyBranch nowyBranch;
+		nowyBranch = new KlasaNowyBranch();
+		nowyBranch.wyswietl();
 	 }		
 }
---
+++ Hello_EGit/src/com/micromate/hello_egit/KlasaNowyBranch.java
@@ -0,0 +1,9 @@
+package com.micromate.hello_egit;
+
+public class KlasaNowyBranch {
+	
+	public void wyswietl(){
+		System.out.println("Klasa zoztała utworona w nowym Branchu");
+	}
+
+}
