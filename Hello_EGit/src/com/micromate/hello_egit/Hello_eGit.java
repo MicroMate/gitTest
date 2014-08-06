de0d264692d28ce6628851ae32966337105ce779
Michal Matczak <matczak.wln@gmail.com>
2014-08-06 11:38:25 +0200

    Tablic int

--- Hello_EGit/src/com/micromate/hello_egit/Hello_eGit.java
+++ Hello_EGit/src/com/micromate/hello_egit/Hello_eGit.java
@@ -3,10 +3,18 @@ package com.micromate.hello_egit;
 public class Hello_eGit {
 	 
 	
-	 static void main (String args[]){ 
+	 public static void main (String args[]){ 
 	
-		System.out.println("Hello eGIT");
+		int tab[] = {10,5,3,8,15,2};
+		 
+		 
+		 System.out.println("Hello eGIT");
 		
 		//Poprawki w Master
+		
+		 //tablica int
+		for (int i =0; i<tab.length; i++){
+			System.out.println(tab[i]);
+		}
 	 }		
 }
